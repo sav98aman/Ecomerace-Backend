c6a030d2ae39c094ccac369e70581d02e0087a71
Aman Kumar <savverma98@gmail.com>
2022-11-16 21:50:18 +0530

    Spring Boot starter complet

---
+++ Ecomerace-backend-Project/src/main/java/com/app/EcomeraceBackendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomeraceBackendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomeraceBackendProjectApplication.class, args);
+	}
+
+}
