811a02a8a9ba2c543aed01150f617e6f29d896ad
Aman Kumar <savverma98@gmail.com>
2022-11-16 22:04:42 +0530

    swagger Enable annoation  add in com.app

--- Ecomerace-backend-Project/src/main/java/com/app/EcomeraceBackendProjectApplication.java
+++ Ecomerace-backend-Project/src/main/java/com/app/EcomeraceBackendProjectApplication.java
@@ -2,8 +2,10 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2//adding swagger to use And explore the ui service 
 public class EcomeraceBackendProjectApplication {
 
 	public static void main(String[] args) {
