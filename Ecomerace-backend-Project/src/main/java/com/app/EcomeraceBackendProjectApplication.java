1010d6ed443eb5cd417df27cc6428037e377f6e3
Aman Kumar <savverma98@gmail.com>
2022-11-16 22:07:48 +0530

    Mysql database Name Change Eco to Ecom

--- Ecomerace-backend-Project/src/main/java/com/app/EcomeraceBackendProjectApplication.java
+++ Ecomerace-backend-Project/src/main/java/com/app/EcomeraceBackendProjectApplication.java
@@ -2,6 +2,7 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
