df3e9d2a050d3b00b4a9fdddaa39f3595a273ab1
Aman Kumar <savverma98@gmail.com>
2022-11-17 12:53:32 +0530

    adding global Excpetion Handaler  and My error Deatils

---
+++ Ecomerace-backend-Project/src/main/java/com/app/Global/exception/GlobalHandlerExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.app.Global.exception;
+
+public class GlobalHandlerExceptionHandler {
+
+}
--- Ecomerace-backend-Project/src/main/java/com/app/Global/exception/MyErrorDeatils.java
+++ Ecomerace-backend-Project/src/main/java/com/app/Global/exception/MyErrorDeatils.java
@@ -1,5 +1,6 @@
 package com.app.Global.exception;
 
 public class MyErrorDeatils {
-
+		
+	
 }
