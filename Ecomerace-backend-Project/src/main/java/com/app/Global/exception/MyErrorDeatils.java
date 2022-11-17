6c981f8c8632a88e5d58c6d237b9a0921db96c2f
Aman Kumar <savverma98@gmail.com>
2022-11-17 12:58:38 +0530

    In MyErrorDeatils feild Is Creating AND AddIng Loombook annotion

--- Ecomerace-backend-Project/src/main/java/com/app/Global/exception/MyErrorDeatils.java
+++ Ecomerace-backend-Project/src/main/java/com/app/Global/exception/MyErrorDeatils.java
@@ -1,6 +1,19 @@
 package com.app.Global.exception;
 
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
 public class MyErrorDeatils {
 		
-	
+	private LocalDateTime timestamp;
+	private String message;
+	private String desc;
 }
