16ef92e3dab85325e867e13b14c08a89106bb085
Aman Kumar <savverma98@gmail.com>
2022-11-18 11:05:35 +0530

    adding new admin Controller Class

---
+++ Ecomerace-backend-Project/src/main/java/com/app/admin/controller/AdminController.java
@@ -0,0 +1,29 @@
+package com.app.admin.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.app.adminservice.AdminServiceImpl;
+import com.app.exception.AdminException;
+import com.app.model.Admin;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
+@RestController
+@RequestMapping(value = "/admin")
+public class AdminController {
+	
+	@Autowired
+	private AdminServiceImpl adminserviceimpl;
+	
+	@PostMapping(value = "/register")
+	public ResponseEntity<Admin> RegisterNewAdmin(@RequestBody Admin admin) throws AdminException{
+		Admin newAdmin=adminserviceimpl.registerNewAdmin(admin);
+		return new ResponseEntity<Admin>(newAdmin,HttpStatus.CREATED);
+	}
+	
+}
