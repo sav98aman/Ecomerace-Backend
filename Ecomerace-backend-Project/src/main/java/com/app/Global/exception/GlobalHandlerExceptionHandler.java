ba0ba8d6ff06606f4781d34f3fc078dd5a7eb015
Aman Kumar <savverma98@gmail.com>
2022-11-18 10:54:09 +0530

    adding ADminException In Globale ExceptionHanaler

--- Ecomerace-backend-Project/src/main/java/com/app/Global/exception/GlobalHandlerExceptionHandler.java
+++ Ecomerace-backend-Project/src/main/java/com/app/Global/exception/GlobalHandlerExceptionHandler.java
@@ -8,8 +8,18 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+import com.app.exception.AdminException;
+
 @ControllerAdvice
 public class GlobalHandlerExceptionHandler {
+	// adding adminGlobale Exceptionhandler
+	
+	@ExceptionHandler(AdminException.class)
+	public ResponseEntity<MyErrorDeatils> AdminExceptiionHandller(AdminException ex,WebRequest req){
+		MyErrorDeatils err=new MyErrorDeatils(LocalDateTime.now(), ex.getMessage(), req.getDescription(false));
+		
+		return new ResponseEntity<MyErrorDeatils>(err,HttpStatus.NOT_FOUND);
+	}
 	
 	@ExceptionHandler(Exception.class)
 	public ResponseEntity<MyErrorDeatils> AllTypeExceptiionHandller(Exception ex,WebRequest req){
