53aed66cda9c3b894913180a1f23adf1e2e66890
Aman Kumar <savverma98@gmail.com>
2022-11-18 10:45:05 +0530

    adding ADminDao Class

---
+++ Ecomerace-backend-Project/src/main/java/com/app/adminLevelDao/AdminDao.java
@@ -0,0 +1,14 @@
+package com.app.adminLevelDao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.app.model.Admin;
+
+@Repository
+public interface AdminDao extends JpaRepository<Admin,Integer> {
+	
+	public Admin findByMobile(String mobile);
+	
+	public Admin findByEmail(String email);	
+}
