f72d34542ff42624bb8d7f2b3cfcd73a5a7d9150
Aman Kumar <savverma98@gmail.com>
2022-11-18 10:21:19 +0530

    admin class make as Entity class

--- Ecomerace-backend-Project/src/main/java/com/app/model/Admin.java
+++ Ecomerace-backend-Project/src/main/java/com/app/model/Admin.java
@@ -1,6 +1,7 @@
 package com.app.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
