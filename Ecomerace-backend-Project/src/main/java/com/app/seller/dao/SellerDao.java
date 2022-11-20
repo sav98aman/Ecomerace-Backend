e5d74904dc86141acf3a4958752e77ed95ab85c5
Aman Kumar <savverma98@gmail.com>
2022-11-20 20:28:21 +0530

    adding seller dao Class

--- Ecomerace-backend-Project/src/main/java/com/app/seller/dao/SellerDao.java
+++ Ecomerace-backend-Project/src/main/java/com/app/seller/dao/SellerDao.java
@@ -1,5 +1,7 @@
 package com.app.seller.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -9,6 +11,6 @@ import com.app.model.Seller;
 @Repository
 public interface SellerDao extends JpaRepository<Seller, Integer> {
 	
-	@Query("SELECT s FROM Seller s WHERE s.seller_Mobile=?1")
+	@Query("SELECT s FROM Seller s WHERE s.seller_Mobile = ?1")
 	public Seller findBySeller_Mobile(String seller_Mobile);
 }
