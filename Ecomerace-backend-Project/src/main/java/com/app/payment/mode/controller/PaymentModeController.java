76e8811e33cb49fa6a85dca770e4c07255517d17
Aman Kumar <savverma98@gmail.com>
2022-11-23 15:23:51 +0530

    adding Payment Controller

---
+++ Ecomerace-backend-Project/src/main/java/com/app/payment/mode/controller/PaymentModeController.java
@@ -0,0 +1,10 @@
+package com.app.payment.mode.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/paymentmode")
+public class PaymentModeController {
+
+}
